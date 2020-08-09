9f3a0e991ef3d946b1f36f304142675e5f562999
Rocky Eastman <eastmanrjr@gmail.com>
2020-08-09 14:11:54 -0700

    Vapor and Gitlab setup

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="UTF-8" />
-    <title>DoDate</title>
+    <title>ToDue</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     @yield('access-token')
