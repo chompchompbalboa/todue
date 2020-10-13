5c6a53ea73673582bfd0e54fa7038856386a8670
eastmanrjr <eastmanrjr@gmail.com>
2020-10-13 14:21:21 -0400

    [Web] Update title

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="UTF-8" />
-    <title>QuickDo - The Todo List That Simplifies Everday Life</title>
+    <title>QuickDo - Fast, Simple Todos</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
     <link rel="stylesheet" type="text/css" href="{{ asset('css/base.css', env('ASSETS_REQUIRE_HTTP') !== null ? !env('ASSETS_REQUIRE_HTTP') : true )}}">
